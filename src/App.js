6e04d62e73060784bd4995c935769a522971ef56
boostup <boostup@gmail.com>
2020-09-14 10:45:55 +0200

    broken the homepage into smaller components + finished styling them

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
-
-import HomePage from "./homepage.component";
+import Homepage from "./pages/HomePage";
 
 class App extends Component {
   render() {
-    return <HomePage />;
+    return <Homepage />;
   }
 }
 
