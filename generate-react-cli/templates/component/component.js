93c2d53885d020d1eaefb4046de7ab9c05676dd1
boostup <boostup@gmail.com>
2020-09-15 14:02:09 +0200

    adding conf and template files for generate-react-cli, a cli to generate component files

---
+++ generate-react-cli/templates/component/component.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./TemplateName.scss";
+
+const TemplateName = () => (
+  <div className="TemplateName">
+    <h1>TemplateName component</h1>
+  </div>
+);
+
+export default TemplateName;
