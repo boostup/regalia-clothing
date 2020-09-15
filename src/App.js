b7d68299cd6e3b38d6f2a04175e2f0bf4176d6f9
boostup <boostup@gmail.com>
2020-09-15 17:26:09 +0200

    user data is now stored in firestore right after a succussful Sign In with Google is performed

--- src/App.js
+++ src/App.js
@@ -6,15 +6,26 @@ import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import ShopPage from "./pages/ShopPage";
 import SignInAndSignOutPage from "./pages/SignInAndSignOutPage";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 function App() {
   const [currentUser, setCurrentUser] = useState(null);
 
   useEffect(() => {
     // listen for auth state changes
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      setCurrentUser(user);
+    const unsubscribe = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        //when user Signs IN
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot((snapShot) => {
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data(),
+          });
+        });
+      }
+      //when user Signs OUT => userAuth === null
+      setCurrentUser(userAuth);
     });
     // unsubscribe to the listener when unmounting
     return () => unsubscribe();
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,6 +2,12 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
+/**
+ * 
+ Typically it is good practice not to expose API key publicly, but in the case of firebase, we have to do so because this is how firebase knows the application is ours! This is perfectly safe, and the intended purpose of this public API key. 
+ However, by committing this code to Github, we may get a warning from GitGuardian having caught a google key, but GitGuardian has acknowledged that this is not an issue [here](https://twitter.com/search?q=firebase%20api%20key%20gitguardian&src=typd)!
+ * 
+ */
 const config = {
   apiKey: "AIzaSyBL3USq_TbUuyH_7auxEHLD3aE6POdGThw",
   authDomain: "crown-clothing-eaab6.firebaseapp.com",
@@ -10,6 +16,31 @@ const config = {
   storageBucket: "crown-clothing-eaab6.appspot.com",
   messagingSenderId: "679771561372",
   appId: "1:679771561372:web:97379b1746df47e5697c59",
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`/users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.error("error creating user", error.message);
+    }
+  }
+  return userRef;
 };
 
 firebase.initializeApp(config);
