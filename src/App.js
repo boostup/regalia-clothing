25fe251039a8c6fe58ff1938d1c57d1a1eb5bdfd
boostup <boostup@gmail.com>
2020-09-15 08:17:15 +0200

    added header component which includes links to the home, shop, and contact pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
+
+import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import ShopPage from "./pages/ShopPage";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+import "./Header.scss";
+
+function Header() {
+  return (
+    <div className="Header">
+      <Link to="/" className="logo-container">
+        <Logo className="logo" />
+      </Link>
+      <div className="options">
+        <Link to="/shop" className="option">
+          Shop
+        </Link>
+        <Link to="/contact" className="option">
+          Contact
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
