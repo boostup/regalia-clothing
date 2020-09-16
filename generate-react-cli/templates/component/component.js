acc457226ec24c7b52ba196a186c192ef540c86f
boostup <boostup@gmail.com>
2020-09-16 10:11:18 +0200

    some minor tweaks

--- generate-react-cli/templates/component/component.js
+++ generate-react-cli/templates/component/component.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./TemplateName.scss";
 
-const TemplateName = () => (
-  <div className="TemplateName">
-    <h1>TemplateName component</h1>
-  </div>
-);
+const TemplateName = () => {
+  return (
+    <div className="TemplateName">
+      <h1>TemplateName component</h1>
+    </div>
+  );
+};
 
 export default TemplateName;
