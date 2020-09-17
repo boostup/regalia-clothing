5192f7509d05107d00db032a47103b18b4a01a6a
boostup <boostup@gmail.com>
2020-09-17 09:51:30 +0200

    Items can be added to Redux state.cart, and are grouped by item.id

--- src/components/CollectionItem/index.jsx
+++ src/components/CollectionItem/index.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import { addItem } from "../../redux/cart/actions";
 
 import CustomButton from "../CustomButton";
 
 import "./CollectionItem.scss";
 
-function CollectionItem({ id, name, price, imageUrl }) {
+function CollectionItem({ item, addItem }) {
+  const { name, price, imageUrl } = item;
+
   return (
     <div className="CollectionItem">
       <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
@@ -12,9 +17,15 @@ function CollectionItem({ id, name, price, imageUrl }) {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <CustomButton inverted>Add to Cart</CustomButton>
+      <CustomButton inverted onClick={() => addItem(item)}>
+        Add to Cart
+      </CustomButton>
     </div>
   );
 }
 
-export default CollectionItem;
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
