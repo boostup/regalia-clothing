6ca45952071fee7cca448c5d94bd35527ffe7f6c
boostup <boostup@gmail.com>
2020-09-17 16:25:33 +0200

    added cart selectors to memoise state slice computations

--- src/components/CartDropdown/CartDropdown.jsx
+++ src/components/CartDropdown/CartDropdown.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { selectCartItems } from "../../redux/cart/selectors";
 
 import CartItem from "../CartItem/CartItem";
 import CustomButton from "../CustomButton";
@@ -19,6 +20,6 @@ const CartDropdown = ({ cartItems }) => {
   );
 };
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({ cartItems });
+const mapStateToProps = (state) => ({ cartItems: selectCartItems(state) });
 
 export default connect(mapStateToProps)(CartDropdown);
