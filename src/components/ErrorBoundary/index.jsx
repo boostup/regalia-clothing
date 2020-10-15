be73fe7d0c5b21b10ecb5748f93933b26fc66e4e
boostup <boostup@gmail.com>
2020-10-15 11:03:00 +0200

    finer ErrorBoundary handling

--- src/components/ErrorBoundary/index.jsx
+++ src/components/ErrorBoundary/index.jsx
@@ -1,31 +1,37 @@
 import React, { Component } from "react";
 
-import { ErrorImageContainer, ErrorImageOverlay, ErrorImageText } from "./error-boundary-styles"
+import {
+  ErrorImageContainer,
+  ErrorImageOverlay,
+  ErrorImageText,
+} from "./error-boundary-styles";
 
 export default class ErrorBoundary extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { hasError: false };
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.log(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <ErrorImageOverlay>
+          <ErrorImageContainer imageUrl="https://i.imgur.com/yW2W9SC.png" />
+          <ErrorImageText>
+            Sorry this page is unvailable at the moment
+          </ErrorImageText>
+        </ErrorImageOverlay>
+      );
     }
 
-    static getDerivedStateFromError(error) {
-        return { hasError: true };
-    }
-
-    componentDidCatch(error, errorInfo) {
-        console.log(error, errorInfo);
-    }
-
-    render() {
-        if (this.state.hasError) {
-            return <ErrorImageOverlay>
-                <ErrorImageContainer imageUrl="https://i.imgur.com/yW2W9SC.png" />
-                <ErrorImageText>
-                    Sorry this page is broken
-                </ErrorImageText>
-            </ErrorImageOverlay>;
-        }
-
-        return this.props.children;
-    }
+    return this.props.children;
+  }
 }
