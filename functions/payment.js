8ce7dfbfae848df1444a641841b3785a1a1a586f
boostup <boostup@gmail.com>
2020-11-24 21:12:48 +0100

    debugging stripeErr format

--- functions/payment.js
+++ functions/payment.js
@@ -14,7 +14,7 @@ exports.handler = async (event) => {
       if (stripeErr) {
         reject({
           statusCode: 500,
-          body: { stripeErr },
+          body: stripeErr,
         });
       } else {
         resolve({
